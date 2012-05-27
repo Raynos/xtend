359600da3b172aa2ae2c7c21694302399362d861
Raynos <raynos2@gmail.com>
2012-05-27 21:38:42 +0100

    clean up code

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = extend
 
 function extend(target) {
-    for (var i = 1, ii = arguments.length; i < ii; i++) {
+    for (var i = 1; i < arguments.length; i++) {
         var source = arguments[i],
             keys = Object.keys(source)
 
-        for (var j = 0, jj = keys.length; j < jj; j++) {
+        for (var j = 0; j < keys.length; j++) {
             var name = keys[j]
             target[name] = source[name]
         }
