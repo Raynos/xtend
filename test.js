d66bb48bfea6f4027c84dd6da5117e6e79c39f00
Roman Shtylman <shtylman@gmail.com>
2013-03-01 21:23:30 -0500

    add tests

---
+++ test.js
@@ -0,0 +1,36 @@
+var test = require('tape');
+var xtend = require('./');
+
+test('merge', function(assert) {
+    var a = { a: 'foo' };
+    var b = { b: 'bar' };
+
+    assert.deepEqual(xtend(a,b), { a: 'foo', b: 'bar' });
+    assert.end();
+});
+
+test('replace', function(assert) {
+    var a = { a: 'foo' };
+    var b = { a: 'bar' };
+
+    assert.deepEqual(xtend(a,b), { a: 'bar' });
+    assert.end();
+});
+
+test('undefined', function(assert) {
+    var a = { a: undefined };
+    var b = { b: 'foo' };
+
+    assert.deepEqual(xtend(a,b), { a: undefined, b: 'foo' });
+    assert.deepEqual(xtend(b,a), { a: undefined, b: 'foo' });
+    assert.end();
+});
+
+test('handle 0', function(assert) {
+    var a = { a: 'default' };
+    var b = { a: 0 };
+
+    assert.deepEqual(xtend(a,b), { a: 0 });
+    assert.deepEqual(xtend(b,a), { a: 'default' });
+    assert.end();
+});
