169b853a2c436d937c3cf2a8edfba1d3b9f359a6
Raynos <raynos2@gmail.com>
2014-04-14 13:35:37 -0700

    remove has-keys. optimize a little

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var hasKeys = require("./has-keys")
-
 module.exports = extend
 
 function extend() {
@@ -7,10 +5,6 @@ function extend() {
 
     for (var i = 0; i < arguments.length; i++) {
         var source = arguments[i]
-
-        if (!hasKeys(source)) {
-            continue
-        }
 
         for (var key in source) {
             if (source.hasOwnProperty(key)) {
--- mutable.js
+++ mutable.js
@@ -1,16 +1,8 @@
-var hasKeys = require("./has-keys")
-
 module.exports = extend
 
 function extend(target) {
-    var sources = [].slice.call(arguments, 1)
-
-    for (var i = 0; i < sources.length; i++) {
-        var source = sources[i]
-
-        if (!hasKeys(source)) {
-            continue
-        }
+    for (var i = 1; i < arguments.length; i++) {
+        var source = arguments[i]
 
         for (var key in source) {
             if (source.hasOwnProperty(key)) {
